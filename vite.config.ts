44ab98234f13727797ed64ba0066889d402debc2
waterbonkboy <114939902+waterbonkboy@users.noreply.github.com>
2026-02-01 15:52:42 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 🔹 ОБЯЗАТЕЛЬНО для GitHub Pages
+  base: '/GamificationSite/',
+
+  // 🔹 если используешь react-router-dom
+  server: {
+    historyApiFallback: true,
+  },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
